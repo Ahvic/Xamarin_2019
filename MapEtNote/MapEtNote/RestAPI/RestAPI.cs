f55ba2f60bd895a1a761282984bd286998c4f38d
Ahvic <34319767+Ahvic@users.noreply.github.com>
2020-02-18 21:10:57 +0100

    2020 2020 2020

---
+++ MapEtNote/MapEtNote/RestAPI/RestAPI.cs
@@ -0,0 +1,48 @@
+﻿using MapAndNotes.Dtos;
+using MonkeyCache.SQLite;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MapEtNote.RestAPI
+{
+    public static class RestAPI
+    {
+        public static async Task<bool> LoginRequest(LoginRequest data)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, Constantes.LoginUrl);
+            request.Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                //On convertie la réponse de l'api en objet utilisable
+                JObject reponseComplete = JObject.Parse(await response.Content.ReadAsStringAsync());
+                string SerializeResult = reponseComplete.Property("data").ToString().Substring(7);
+
+                LoginResult result = JsonConvert.DeserializeObject<LoginResult>(SerializeResult);
+
+                //On stocke le refreshToken
+                Barrel.Current.Add(key: "accesToken", data: result.AccessToken, expireIn: TimeSpan.FromDays(result.ExpiresIn));
+                Barrel.Current.Add(key: "refreshToken", data: result.RefreshToken, expireIn: TimeSpan.FromDays(result.ExpiresIn));
+
+                //await Application.Current.MainPage.DisplayAlert("Alerte", "Connecté ! " + result.ToString(), "OK");
+
+                return true;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("réponse login perdu");
+                return false;
+            }
+        }
+    }
+}
---
+++ MapEtNote/MapEtNote/ViewModel/LoginModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Xamarin.Forms;
+using Storm.Mvvm;
+using Newtonsoft.Json;
+using MapAndNotes.Dtos;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using MapEtNote;
+using MonkeyCache.SQLite;
+using MapEtNote.RestAPI;
+
+namespace MapEtNotes.Models
+{
+    public class LoginModel : ViewModelBase
+    {
+        private string _username;
+        private string _password;
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty<string>(ref _username, value); }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty<string>(ref _password, value); }
+        }
+
+        public ICommand SubmitCommand { get; set; }
+
+        public LoginModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+            Barrel.ApplicationId = "coucou";
+        }
+
+        public async void OnSubmit()
+        {
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Alerte", "Username ou mot de passe vide", "OK");
+            }
+            else
+            {
+                LoginRequest user = new LoginRequest() {
+                    Email = Username,
+                    Password = Password
+                };
+
+                if (await RestAPI.LoginRequest(user))
+                {
+                    await Application.Current.MainPage.DisplayAlert("Alerte", "Login correct", "OK");
+
+                    await Application.Current.MainPage.DisplayAlert("Alerte", "Bonjour " + Barrel.Current.Get<string>(key: "refreshToken"), "OK");
+
+                    //Ouverture fenêtre
+                }
+                else 
+                {
+                    await Application.Current.MainPage.DisplayAlert("Alerte", "Login incorrect", "OK");
+                }
+            }
+        }
+    }
+}
